e8b7f44f9eb83407fd7f5d46d4a39be82c196a91
temucher <tjsmuch@gmail.com>
2014-10-24 11:04:28 -0700

    OldMacDonald

--- build-tmp/source/OldMacDonald.java
+++ build-tmp/source/OldMacDonald.java
@@ -1,4 +1,18 @@
-//Teagan Mucher, Block 4 AP CS, "OldMacDonald"
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class OldMacDonald extends PApplet {
 
 public void setup()
 {
@@ -46,7 +60,7 @@ class Chick implements Animal
 	public Chick(String type, String sound1, String sound2)
 	{
 		myType = type; 
-		if (Math.random()<=.5)
+		if (Math.random()<=.5f)
 		{        
      		mySound = sound1;
      	}
@@ -97,4 +111,13 @@ class Farm
     }    
     System.out.println( "The cow is known as " + ((namedCow)aBunchOfAnimals[0]).getName() );
   } 
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "OldMacDonald" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
